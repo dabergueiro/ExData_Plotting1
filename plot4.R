90c76e969149c7696a2c8099513a6a3368152c5b
dabergueiro <da_bergueiro@hotmail.com>
2014-08-10 15:52:14 -0600

    quick fix

--- plot4.R
+++ plot4.R
@@ -17,7 +17,8 @@ with(subset, plot(subset$NewDate, as.numeric(as.character(subset$Sub_metering_1)
 with(subset, points(subset$NewDate, as.numeric(as.character(subset$Sub_metering_1)), type="l", col="black"))
 with(subset, points(subset$NewDate, as.numeric(as.character(subset$Sub_metering_2)), type="l", col="red"))
 with(subset, points(subset$NewDate, as.numeric(as.character(subset$Sub_metering_3)), type="l", col="blue"))
-legend("topright", pch="-" , col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+#legend("topright", col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lwd="0")
+legend("topright", col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lwd="0", bty="n")
 
 #Plot3
 plot(subset$NewDate, as.numeric(as.character(subset$Voltage)), type="l", xlab="datetime", ylab="Voltage")
