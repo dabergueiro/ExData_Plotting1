b215f27bfcfed0c3f1687fa7f507330846867782
dabergueiro <da_bergueiro@hotmail.com>
2014-08-10 13:12:00 -0600

    first commit

---
+++ plot1.R
@@ -0,0 +1,5 @@
+data <- read.table("household_power_consumption.txt", header=T, sep=";")
+subset <- data[data["Date"]=="1/2/2007" | data["Date"]=="2/2/2007",]
+png(filename="plot1.png")
+hist(as.numeric(as.character(subset[,"Global_active_power"])), main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+dev.off()
